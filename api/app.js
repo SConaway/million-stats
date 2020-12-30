eba12711ccf0967fe4b9ff0ba2bcc3090edc0ed1
Steven Conaway <steven.conaway@icloud.com>
2020-12-30 12:14:07 -0800

    initial change with a bunch of `console.log()`s

--- api/app.js
+++ api/app.js
@@ -1,17 +1,17 @@
 // Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+const {App} = require('@slack/bolt');
 const keep_alive = require('./keep_alive.js');
 const addQuotes = require('./quotes.js');
 const schedule = require('node-schedule');
 const moment = require('moment');
 const token = process.env.SLACK_BOT_TOKEN;
-const channel = "CDJMS683D"; 
-const Airtable = require('airtable');
-Airtable.configure({
-	endpointUrl: 'https://api.airtable.com',
-	apiKey: process.env.AIRTABLE_API_KEY
-});
-const base = Airtable.base('appogmRaVRo5ElVH7');
+const channel = 'C01HYCWEJ5S';
+// const Airtable = require('airtable');
+// Airtable.configure({
+// 	endpointUrl: 'https://api.airtable.com',
+// 	apiKey: process.env.AIRTABLE_API_KEY
+// });
+// const base = Airtable.base('appogmRaVRo5ElVH7');
 let speedArr = [];
 let latest;
 let averageSpeed;
@@ -20,14 +20,14 @@ addQuotes();
 
 const app = new App({
 	token: token,
-	signingSecret: process.env.SLACK_SIGNING_SECRET
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 function extractNumber(txt) {
-	let array = ["\n", " ", "-"]
+	let array = ['\n', ' ', '-'];
 	for (let i of array) {
 		if (txt.includes(i)) {
-			return txt.split(i)[0]
+			return txt.split(i)[0];
 		}
 	}
 	return txt;
@@ -40,7 +40,7 @@ async function fetchLatest(id) {
 			channel: id,
 			limit: 1,
 		});
-		const number = extractNumber(result.messages[0].text)
+		const number = extractNumber(result.messages[0].text);
 		return number;
 	} catch (error) {
 		console.error(error);
@@ -53,10 +53,23 @@ async function fetchOldest(id) {
 		const result = await app.client.conversations.history({
 			token: token,
 			channel: id,
-			oldest: Math.floor(Date.now() / 1000) - 86400, //debug: 1596326400, actual: Math.floor(Date.now() / 1000) - 86400
-			inclusive: false
+			oldest: Math.floor(Date.now() / 1000) - 86400, //debug: 1609295166, actual: Math.floor(Date.now() / 1000) - 86400
+			inclusive: false,
 		});
-		const number = extractNumber(result.messages[result.messages.length - 2].text);
+		console.log(result.messages);
+		let number;
+		for (let x = 2; x < result.messages.length; x++) {
+			console.log(
+				`x: ${x}; message=${JSON.stringify(
+					result.messages[result.messages.length - x],
+				)}`,
+			);
+			number = extractNumber(
+				result.messages[result.messages.length - x].text,
+			);
+
+			if (!isNaN(number)) break;
+		}
 		return number - 1;
 	} catch (error) {
 		console.error(error);
@@ -68,7 +81,7 @@ async function publishMessage(id, text) {
 		const result = await app.client.chat.postMessage({
 			token: token,
 			channel: id,
-			text: text
+			text: text,
 		});
 	} catch (error) {
 		console.error(error);
@@ -81,10 +94,10 @@ async function postReaction(id, emoji, ts) {
 			token: token,
 			channel: id,
 			name: emoji,
-			timestamp: ts
+			timestamp: ts,
 		});
 	} catch (error) {
-		console.error(error)
+		console.error(error);
 	}
 }
 
@@ -93,10 +106,10 @@ async function pinMessage(id, ts) {
 		const result = await app.client.pins.add({
 			token: token,
 			channel: id,
-			timestamp: ts
-		})
+			timestamp: ts,
+		});
 	} catch (error) {
-		console.error(error)
+		console.error(error);
 	}
 }
 
@@ -108,64 +121,65 @@ function findMean(arr) {
 	return totalSum / arr.length;
 }
 
-async function addData(db, object) {
-	base(db).create(object, function(err, record){
-		if (err) {
-			console.error(err);
-			return;
-		}
-		// console.log(record.getId());
-	})
-}
+// async function addData(db, object) {
+// 	base(db).create(object, function(err, record){
+// 		if (err) {
+// 			console.error(err);
+// 			return;
+// 		}
+// 		// console.log(record.getId());
+// 	})
+// }
 
-async function getStats() {
-	try {
-		let obj = await base('stats').find('rec2XI8QAsPr7EMVB');
-		return {
-        id: obj.id,
-        fields: obj.fields,
-    };
-	} catch (error) {
-		console.error(error)
-	}
-}
+// async function getStats() {
+// 	try {
+// 		let obj = await base('stats').find('rec2XI8QAsPr7EMVB');
+// 		return {
+//         id: obj.id,
+//         fields: obj.fields,
+//     };
+// 	} catch (error) {
+// 		console.error(error)
+// 	}
+// }
 
 async function report() {
 	let oldest = await fetchOldest(channel);
 	let latest = await fetchLatest(channel);
 	let diff = latest - oldest;
-	addData('increase', {
-		"Date": moment().subtract(1, "days").format("YYYY-MM-DD"),
-		"increase": diff,
-		"stats": [
-        "rec2XI8QAsPr7EMVB"
-      ]
-	})
-	let newStats = await getStats();
-	averageSpeed = newStats.fields.average.toFixed(3);
+	// addData('increase', {
+	// 	"Date": moment().subtract(1, "days").format("YYYY-MM-DD"),
+	// 	"increase": diff,
+	// 	"stats": [
+	//       "rec2XI8QAsPr7EMVB"
+	//     ]
+	// })
+	// let newStats = await getStats();
+	// averageSpeed = newStats.fields.average.toFixed(3);
 	let thousandsGoal = Math.ceil(latest / 1000) * 1000;
 	let thousandsTime = predictTime(thousandsGoal, latest);
 	let tenThousandsGoal = Math.ceil(latest / 5000) * 5000;
 	let pastThousandsGoal = Math.floor(latest / 1000) * 1000;
 	let tenThousandsTime = predictTime(tenThousandsGoal, latest);
-	let message =
-		`Good evening, my underlings! You have done a _brilliant_ job with the counting. Mmm. Yes. Numbers are savory. 
+	let message = `Good evening, my underlings! You have done a _brilliant_ job with the counting. Mmm. Yes. Numbers are savory. 
 		Today we've went from *${oldest}* to *${latest}*!
 		- :arrow_upper_right: The day's progress: *+${diff}*
-		- :chart_with_upwards_trend: Average daily speed: *${averageSpeed}*
+		- :chart_with_upwards_trend: Average daily speed: *averageSpeed*
 		- :round_pushpin: At the avg speed, we'll reach ${thousandsGoal} *${thousandsTime}* 
 		- :calendar: At the avg speed, we'll reach ${tenThousandsGoal} *${tenThousandsTime}* 
 		:bat: Lovely work, my followers! Keep on quenching my thirst for numbers!`;
-	if (pastThousandsGoal > oldest && pastThousandsGoal <= latest) {
-		let messageWithCelebration = `:tada: Congratulations! We've went past ${pastThousandsGoal}! :tada:` + message;
-		publishMessage(channel, messageWithCelebration); 
-		// publishMessage('C017W4PHYKS', messageWithCelebration);
-	} else {
-		publishMessage(channel, message);
-		// publishMessage('C017W4PHYKS', message);
-	}
-
-};
+	console.log(message);
+	// if (pastThousandsGoal > oldest && pastThousandsGoal <= latest) {
+	// 	let messageWithCelebration =
+	// 		`:tada: Congratulations! We've went past ${pastThousandsGoal}! :tada:` +
+	// 		message;
+	// 	publishMessage(channel, messageWithCelebration);
+	// 	// publishMessage('C017W4PHYKS', messageWithCelebration);
+	// } else {
+	// 	publishMessage(channel, message);
+	// 	// publishMessage('C017W4PHYKS', message);
+	// }
+}
 
 function predictTime(goal, recent) {
 	let daysLeft = (goal - recent) / averageSpeed;
@@ -176,19 +190,19 @@ function predictTime(goal, recent) {
 app.event('message', async (body) => {
 	try {
 		let e = body.event;
-		if (typeof e.subtype === "undefined" && /\d/.test(e.text[0])) {
+		if (typeof e.subtype === 'undefined' && /\d/.test(e.text[0])) {
 			let number = extractNumber(e.text);
 			let ts = e.ts;
 			let c = e.channel;
 			if (number % 1000 === 0) {
-				postReaction(c, "tada", ts);
+				postReaction(c, 'tada', ts);
 			}
 			if (number % 5000 === 0) {
 				pinMessage(c, ts);
 			}
 			let l = number.length;
 			if (number[l - 2] == 6 && number[l - 1] == 9) {
-				postReaction(c, "ok_hand", ts);
+				postReaction(c, 'ok_hand', ts);
 			}
 		}
 	} catch (err) {
@@ -201,6 +215,7 @@ app.event('message', async (body) => {
 	try {
 		await app.start(process.env.PORT || 3000);
 		let j = schedule.scheduleJob('0 0 * * *', report);
+		report();
 		// let j = schedule.scheduleJob('*/15 * * * * *', report);
 	} catch (error) {
 		console.error(error);
